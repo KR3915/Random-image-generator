ef96c32b8a77fa375712d27b50cc94de4c23c405
KR3915 <ondrej.brdek@gmail.com>
2025-06-09 15:29:25 +0200

    show img

--- image generator.py
+++ image generator.py
@@ -1,13 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def make_img(#height, width, channel
-        ):
+
+def make_img():
     rnd_arr = np.random.rand(35, 35, 3)
     return rnd_arr
+
 def display_img(array):
+    
     plt.imshow(array)
     plt.show()
+
 def main():
-    display_img(array=make_img())
-
-
+    for i in range(10):
+        display_img(array=make_img())
+if __name__ == "__main__":
+    main()
